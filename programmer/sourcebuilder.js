0efc6937443882db4b91df644ccae0d0ff27c827
Julien <juliendorra@juliendorra.com>
2015-10-30 02:01:17 +0100

    insert the code from users into the .pde template
    kids can just code their creature without having to care about the
    boilerplate. This utility function get the .pde template, replace a
    comment in it with their creature code and return the resulting source
    code.

---
+++ programmer/sourcebuilder.js
@@ -0,0 +1,26 @@
+getCompleteSource = function(codefragment) {
+
+  $.get(
+
+    "creatureclass.pde",
+
+    function(text) {
+
+      console.log(text);
+
+      var template = text;
+      var usercode = codefragment;
+
+      var match = /\/\/kids code here/;
+
+      var sourcecode = template.replace(match, usercode);
+      console.log(sourcecode);
+
+      return sourcecode;
+
+
+    },
+
+    "text");
+
+};
